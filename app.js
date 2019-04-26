12e23520bf0044d2de20659f66ca4c0223e4ab12
pushpendrakumar <pushpendra.kumar@b2x.com>
2019-04-26 10:11:23 +0530

    update travis & add docker ignore file

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const events = require('events')
+const eventsEmitter = new events.EventEmitter()
 const port = 3000
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  eventsEmitter.emit('scream')
+  // when we need to inform someone about certain task in this case events is good
+  res.send('Hello World!')
+})
+let myEventsHandler = function () {
+  console.log('i heard scream')
+}
+eventsEmitter.on('scream', myEventsHandler)
 app.get('/user', require('./app/controller/userController').userDetails)
+app.get('/sendMail', require('./app/controller/userController').sendMail)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 module.exports = app
--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,5 +1,27 @@
+var url = require('url')
+const nodeMailer = require('nodemailer')
 
-function userDetails (req, res) {
-  res.status(200).send({'firstName': 'Pushpendra', 'lastName': 'Kumar'})
+function userDetails(req, res) {
+  res.status(200).send(url.parse(req.url, true).query)
 }
-module.exports.userDetails = userDetails
+
+function sendMail() {
+  let transport = nodeMailer.createTransport({
+    'service': 'gmail',
+    'auth': {
+      'user': 'pushpendra.bhumca2013@gmail.com',
+      'password': 'pushpVK@91'
+    }
+  })
+  let mailOpetions = {
+    from: 'pushpendra.bhumca2013@gmail.com',
+    to: 'pushpendra.bhumca2013@gmail.com'
+  }
+  transport.sendMail(mailOpetions, function (error, response) {
+    if (error) { console.log(error) } else { console.log(response) }
+  })
+}
+module.exports = {
+  userDetails: userDetails,
+  sendMail: sendMail
+}
